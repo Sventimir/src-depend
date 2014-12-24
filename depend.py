ad0dc5f8c78461a45ebe6286a8d746a4c648a164
Sventimir <svantimir@gmail.com>
2014-12-24 22:50:57 +0100

    Added --exclude-regex option.

--- depend.py
+++ depend.py
@@ -3,11 +3,12 @@ from source code itself. It iterates through all source code files in given
 directory, finds import statements and turns them into edges of a dependency
 graph.
 
-uses graphviz for sketching graphs."""
+Uses graphviz for sketching graphs."""
 import argparse
 import graphviz
 import logging
 import os.path
+import re
 
 
 def parseargs():
@@ -16,7 +17,9 @@ def parseargs():
     parser.add_argument('-s', '--source', dest='src_out')
     parser.add_argument('-i', '--image', dest='img_out')
     parser.add_argument('-d', '--debug', dest='debug', action='store_true')
-    parser.add_argument('-e', '--exclude', dest='exclude', nargs='+', default=[])
+    parser.add_argument('-f', '--output-format', dest='format', default='png')
+    parser.add_argument('-e', '--exclude', dest='excludes', nargs='+', default=[])
+    parser.add_argument('--exclude-regex', dest='exclude-regex', default=None)
     parser.add_argument('-q', '--quiet', dest='quiet', action='store_true')
     parser.add_argument('target')
     return parser.parse_args().__dict__
@@ -32,20 +35,24 @@ def main(args):
             level=log_level,
             format='[%(asctime)s; %(levelname)s]: %(message)s'
         )
+
+    is_excluded = exclude_checker(args['excludes'], args['exclude-regex'])
+
     try:
         import_obj = __import__('plugins.{}'.format(args['lang']))
         plugin = getattr(import_obj, args['lang'])
     except ImportError:
-        logging.error('Could not find module {}!'.format(args['lang']))
+        logging.error('Could not find plugin for {}!'.format(args['lang']))
         return 1
-    for f in find_source_files(
-            args['target'], plugin.Module.filename_ext, args['exclude']
-        ):
+
+    files = find_source_files(args['target'], plugin.Module.filename_ext, is_excluded)
+    for f in files:
         with open(f, 'r') as file:
             plugin.Module(file)
+
     plugin.Module.create_dependency_tree()
     graph = make_graph(*plugin.Module.registry)
-    graph.format = 'png'
+    graph.format = args['format']
     write_file(args['src_out'], graph.source, 'source')
 
     if not args['img_out'] is None:
@@ -67,9 +74,9 @@ def make_graph(*modules):
     return graph
 
 
-def find_source_files(path, ext, excludes):
+def find_source_files(path, ext, is_excluded):
     basename = os.path.basename(path)
-    if basename in excludes:
+    if is_excluded(basename):
         logging.debug('Ommitting excluded path: {}...'.format(path))
     elif not basename == '.' and basename.startswith('.'):
         logging.debug('Ommitting hidden path: {}...'.format(path))
@@ -79,7 +86,7 @@ def find_source_files(path, ext, excludes):
     elif os.path.isdir(path):
         logging.debug('In dir "{}": {}'.format(path, os.listdir(path)))
         for f in os.listdir(path):
-            for el in find_source_files(os.path.join(path, f), ext, excludes):
+            for el in find_source_files(os.path.join(path, f), ext, is_excluded):
                 yield el
     else:
         logging.debug('{} is not a source file.'.format(path))
@@ -93,6 +100,15 @@ def write_file(file, content, what):
                 file.writelines(content)
         except IOError:
             logging.error('Could not write to: {}!'.format(file))
+
+
+def exclude_checker(excluded, regex):
+    if regex is None:
+        return lambda filename: filename in excluded
+    else:
+        compiled_regex = re.compile(regex)
+        return lambda filename:filename in excluded \
+                or compiled_regex.match(filename)
 
 
 if __name__ == '__main__':
